66ea916849201159234d6db5522a696043dd0eb1
rose-rawal <rooserwl@gmail.com>
2024-05-29 00:06:14 +0545

    Add project files.

---
+++ RunGroopWebApp/Models/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RunGroopWebApp.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public int? Pace { get; set; }
+        public int? Mileage { get; set; }
+        public Address? Address { get; set; }
+
+        public ICollection<Club> Clubs { get; set; }
+        public ICollection<Race> Races { get; set; }
+    }
+}
